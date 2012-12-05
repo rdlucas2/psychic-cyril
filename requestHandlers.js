2f483a0c15e121d9e590b92f914bb9575e926701
Ryan D. Lucas <rdlucas2@gmail.com>
2012-12-04 23:55:42 -0500

    init

---
+++ requestHandlers.js
@@ -0,0 +1,37 @@
+var exec = require("child_process").exec;
+var fs = require('fs');
+
+function open(response, postData) {
+	fs.readFile('./site/index.html', function(error, html) {
+		if(error) {
+			throw error;
+	}
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write(html);
+		response.end();
+	});
+}
+
+function start(response) {
+	console.log("Request handler 'start' was called.");
+
+	exec("ls -lah", function(error, stdout, stderr) {
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
+	});
+}
+
+var querystring = require("querystring");
+
+function upload(response, postData) {
+	console.log("Request handler 'upload' was called.");
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("You've sent the text: "+
+	querystring.parse(postData).text);
+	response.end();
+}
+
+exports.open = open;
+exports.start = start;
+exports.upload = upload;
---
+++ server.js
@@ -0,0 +1,78 @@
+var http = require("http");
+var url = require("url");
+var fs = require('fs');
+
+function start(route, handle) {
+    function onRequest(request, response) {
+        var postData = "";
+        var pathname = url.parse(request.url).pathname;
+
+        var filePath = '.' + request.url;
+        if (filePath == './') {
+            filePath = './site/index.html';
+        } else {
+            filePath = './site' + request.url;
+        }
+
+        console.log("\nRequest for " + pathname + " received.");
+
+        if (fs.existsSync(filePath)) {
+            fs.exists(filePath, function (exists) {
+
+                if (exists) {
+                    fs.readFile(filePath, function (error, content) {
+                        if (error) {
+                            console.log("There was an error loading the file: " + error);
+                            response.writeHead(500);
+                            response.end();
+                        }
+                        else {
+                            console.log("Request is a valid file - loading the file...");
+                            response.writeHead(200, { 'Content-Type': 'text/html' });
+                            response.end(content, 'utf-8');
+                        }
+                    });
+                }
+                else {
+                    console.log("Valid file but 404'd???");
+                    response.writeHead(404);
+                    response.end();
+                }
+            });
+        }
+        else {
+            request.setEncoding("utf8");
+
+            request.addListener("data", function (postDataChunk) {
+                postData += postDataChunk;
+                console.log("Received POST data chunk '" +
+                postDataChunk + "'.");
+            });
+
+            request.addListener("end", function () {
+                route(handle, pathname, response, postData);
+            });
+        }
+    }
+    var app = http.createServer(onRequest).listen(8888);
+    console.log("\nServer has started.");
+
+    io = require('socket.io').listen(app);
+    io.sockets.on('connection', function (socket) {
+
+        socket.on('set nickname', function (name) {
+            socket.set('nickname', name, function () { socket.emit('ready'); });
+        });
+
+        socket.on('msg', function (data) {
+            socket.get('nickname', function (err, name) {
+                console.log('Chat message by ', name);
+                io.sockets.emit('user', { user: name });
+                io.sockets.emit('sent', { sent: data });
+            });
+        });
+
+    });
+}
+
+exports.start = start;
