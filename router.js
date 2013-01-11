28d51ff9ea8c38c6cf2e684dc631c5e3cdedd0b9
Ryan D. Lucas <rdlucas2@gmail.com>
2013-01-10 22:53:21 -0500

    cleaning up a few files, better 404 handling

--- router.js
+++ router.js
@@ -1,12 +1,22 @@
+var fs = require('fs');
+
 function route(handle, pathname, response, postData) {
 	console.log("About to route a request for " + pathname);
 	if (typeof handle[pathname] === 'function') {
 		return handle[pathname](response, postData);
 	} else {
-		console.log("No request handler found for " + pathname);
-		response.writeHead(404, {"Content-Type": "text/plain"});
-		response.write("404 Not Found");
-		response.end();
+	    console.log("No request handler found for " + pathname);
+		response.writeHead(404, { "Content-Type": "text/html" });
+		fs.readFile('./site/404.html', function (error, html) {
+		    if (error) {
+		        console.log("There was an error loading the file: " + error);
+		        response.writeHead(500);
+		        response.end();
+		    }
+		    response.writeHead(200, { "Content-Type": "text/html" });
+		    response.write(html);
+		    response.end();
+		});
 	}
 }
 
